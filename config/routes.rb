3d16b625894de8c93d09ca2e0bc87fd6b108cdf6
ryo-suzuki-seamagic <r40suzu@gmail.com>
2020-07-24 15:46:57 +0900

    新規投稿画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "messages#index"
   resources :users, onry: [:ebit, :update]
+  resources :rooms, onry: [:new, :create]
+
 end
